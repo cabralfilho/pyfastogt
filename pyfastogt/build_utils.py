fad931c727e8bb29af58790683ad98d8e74a5dc0
topilski <atopilski@gmail.com>
2019-05-05 21:26:32 +0300

    Removed compileinfo

--- pyfastogt/build_utils.py
+++ pyfastogt/build_utils.py
@@ -1,6 +1,5 @@
 import os
 import stat
-import re
 import shutil
 import subprocess
 from pyfastogt import system_info, utils
@@ -39,21 +38,6 @@ class BuildError(Exception):
         return self.value_
 
 
-class CompileInfo(object):
-    def __init__(self, patches: list, flags: list):
-        self.patches_ = patches
-        self.flags_ = flags
-
-    def patches(self):
-        return self.patches_
-
-    def flags(self) -> list:
-        return self.flags_
-
-    def extend_flags(self, other_args):
-        self.flags_.extend(other_args)
-
-
 # must be in cmake folder
 def build_command_cmake(prefix_path: str, cmake_flags: list, build_type='RELEASE',
                         build_system=get_supported_build_system_by_name('ninja')):
@@ -85,23 +69,14 @@ def build_command_cmake(prefix_path: str, cmake_flags: list, build_type='RELEASE
 
 
 # must be in configure folder
-def build_command_configure(compiler_flags: CompileInfo, patch_dir_path, prefix_path, executable='./configure',
+def build_command_configure(compiler_flags: list, prefix_path, executable='./configure',
                             build_system=get_supported_build_system_by_name('make')):
     # +x for exec file
     st = os.stat(executable)
     os.chmod(executable, st.st_mode | stat.S_IEXEC)
 
-    for file_names in compiler_flags.patches():
-        scan_dir = os.path.join(patch_dir_path, file_names)
-        if os.path.exists(scan_dir):
-            for diff in os.listdir(scan_dir):
-                if re.match(r'.+\.patch', diff):
-                    patch_file = os.path.join(scan_dir, diff)
-                    line = 'patch -p0 < {0}'.format(patch_file)
-                    subprocess.call(['bash', '-c', line])
-
     compile_cmd = [executable, '--prefix={0}'.format(prefix_path)]
-    compile_cmd.extend(compiler_flags.flags())
+    compile_cmd.extend(compiler_flags)
     subprocess.call(compile_cmd)
     make_line = build_system.cmd_line()
     subprocess.call(make_line)
@@ -168,11 +143,11 @@ class BuildRequest(object):
         self._clone_and_build_via_cmake(generate_fastogt_git_path('json-c'), ['-DBUILD_SHARED_LIBS=OFF'])
 
     def build_libev(self):
-        libev_compiler_flags = CompileInfo([], ['--with-pic', '--disable-shared', '--enable-static'])
+        libev_compiler_flags = ['--with-pic', '--disable-shared', '--enable-static']
         self._clone_and_build_via_autogen(generate_fastogt_git_path('libev'), libev_compiler_flags)
 
     def build_cpuid(self):
-        cpuid_compiler_flags = CompileInfo([], ['--disable-shared', '--enable-static'])
+        cpuid_compiler_flags = ['--disable-shared', '--enable-static']
 
         pwd = os.getcwd()
         cloned_dir = utils.git_clone(generate_fastogt_git_path('libcpuid'))
@@ -200,7 +175,7 @@ class BuildRequest(object):
         self._clone_and_build_via_cmake(generate_fastogt_git_path('common'), cmake_flags)
 
     def build_openssl(self, version):
-        compiler_flags = CompileInfo([], ['no-shared', 'no-unit-test'])
+        compiler_flags = ['no-shared', 'no-unit-test']
         url = '{0}openssl-{1}.{2}'.format(self.OPENSSL_SRC_ROOT, version, self.ARCH_OPENSSL_EXT)
         self._download_and_build_via_configure(url, compiler_flags, './config')
 
@@ -216,14 +191,14 @@ class BuildRequest(object):
         self._build_via_cmake(cmake_flags)
         os.chdir(pwd)
 
-    def _clone_and_build_via_configure(self, url: str, compiler_flags: CompileInfo, branch=None, remove_dot_git=True):
+    def _clone_and_build_via_configure(self, url: str, compiler_flags: list, branch=None, remove_dot_git=True):
         pwd = os.getcwd()
         cloned_dir = utils.git_clone(url, branch, remove_dot_git)
         os.chdir(cloned_dir)
         self._build_via_configure(compiler_flags)
         os.chdir(pwd)
 
-    def _clone_and_build_via_autogen(self, url: str, compiler_flags: CompileInfo, executable='./configure', branch=None,
+    def _clone_and_build_via_autogen(self, url: str, compiler_flags: list, executable='./configure', branch=None,
                                      remove_dot_git=True):
         pwd = os.getcwd()
         cloned_dir = utils.git_clone(url, branch, remove_dot_git)
@@ -240,7 +215,7 @@ class BuildRequest(object):
         self._build_via_cmake(cmake_flags)
         os.chdir(pwd)
 
-    def _download_and_build_via_autogen(self, url: str, compiler_flags: CompileInfo, executable='./configure'):
+    def _download_and_build_via_autogen(self, url: str, compiler_flags: list, executable='./configure'):
         pwd = os.getcwd()
         file_path = utils.download_file(url)
         extracted_folder = utils.extract_file(file_path)
@@ -248,7 +223,7 @@ class BuildRequest(object):
         self._build_via_autogen(compiler_flags, executable)
         os.chdir(pwd)
 
-    def _download_and_build_via_configure(self, url: str, compiler_flags: CompileInfo, executable='./configure'):
+    def _download_and_build_via_configure(self, url: str, compiler_flags: list, executable='./configure'):
         pwd = os.getcwd()
         file_path = utils.download_file(url)
         extracted_folder = utils.extract_file(file_path)
@@ -257,7 +232,7 @@ class BuildRequest(object):
         os.chdir(pwd)
 
     # build
-    def _build_via_autogen(self, compiler_flags: CompileInfo, executable='./configure'):
+    def _build_via_autogen(self, compiler_flags: list, executable='./configure'):
         autogen_line = ['sh', 'autogen.sh']
         subprocess.call(autogen_line)
         self._build_via_configure(compiler_flags, executable)
@@ -268,5 +243,5 @@ class BuildRequest(object):
         cmake_flags_extended.extend(self.platform_.cmake_specific_flags())
         build_command_cmake(self.prefix_path_, cmake_flags)
 
-    def _build_via_configure(self, compiler_flags: CompileInfo, executable='./configure'):
+    def _build_via_configure(self, compiler_flags: list, executable='./configure'):
         build_command_configure(compiler_flags, self.patch_path_, self.prefix_path_, executable)
